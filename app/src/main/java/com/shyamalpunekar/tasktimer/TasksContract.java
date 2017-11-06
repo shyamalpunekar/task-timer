7e90a5e74b0384499a1516fc3c26ae6f86424d54
shy <shyamal.punekar@gmail.com>
2017-11-06 15:59:01 -0800

    Add Contract class to provide constants

---
+++ app/src/main/java/com/shyamalpunekar/tasktimer/TasksContract.java
@@ -0,0 +1,23 @@
+package com.shyamalpunekar.tasktimer;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by spunek on 11/6/17.
+ */
+
+public class TasksContract {
+    static final String TABLE_NAME = "Tasks";
+
+    public static class Columns {
+        public static final String _ID = BaseColumns._ID;
+        public static final String TASKS_NAME = "Name";
+        public static final String TASKS_DESCRIPTION = "Description";
+        public static final String TASKS_SORTORDER = "SortOrder";
+
+        private Columns(){
+
+        }
+    }
+
+}
